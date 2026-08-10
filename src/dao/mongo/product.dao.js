30f92fd2618c12c6e6ef97b557387c79ce933c07
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-08-10 16:05:32 -0300

    dao: improve product persistence operations

--- src/dao/mongo/product.dao.js
+++ src/dao/mongo/product.dao.js
@@ -1,23 +1,22 @@
 import { productModel } from '../models/product.model.js';
 
 export default class ProductMongoDAO {
-    async get() {
-        return await productModel.find();
+    async get({ page = 1, limit = 10, sort, category, availability } = {}) {
+        const filter = {};
+        if (category) filter.category = category;
+        if (availability !== undefined) filter.stock = availability === 'true' ? { $gt: 0 } : 0;
+
+        const options = {
+            page: Number(page),
+            limit: Number(limit),
+            sort: sort === 'asc' ? { price: 1 } : sort === 'desc' ? { price: -1 } : undefined
+        };
+
+        return productModel.paginate(filter, options);
     }
 
-    async getById(id) {
-        return await productModel.findById(id);
-    }
-
-    async create(product) {
-        return await productModel.create(product);
-    }
-
-    async update(id, product) {
-        return await productModel.findByIdAndUpdate(id, product, { new: true });
-    }
-
-    async delete(id) {
-        return await productModel.findByIdAndDelete(id);
-    }
+    async getById(id) { return productModel.findById(id); }
+    async create(product) { return productModel.create(product); }
+    async update(id, product) { return productModel.findByIdAndUpdate(id, product, { new: true, runValidators: true }); }
+    async delete(id) { return productModel.findByIdAndDelete(id); }
 }
