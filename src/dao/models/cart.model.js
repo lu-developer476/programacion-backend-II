d6d8a4fd989563eb2a2a8b7b06a7b141067ba04a
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-08-10 16:06:14 -0300

    model: simplify cart references for transactional operations

--- src/dao/models/cart.model.js
+++ src/dao/models/cart.model.js
@@ -2,20 +2,16 @@ import mongoose from 'mongoose';
 
 const cartCollection = 'carts';
 
-const cartSchema = new mongoose.Schema({
-    products: [
-        {
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'products'
-            },
-            quantity: { type: Number, default: 1 }
-        }
-    ]
-});
-
-cartSchema.pre('findOne', function() {
-    this.populate('products.product');
-});
+const cartSchema = new mongoose.Schema(
+    {
+        products: [
+            {
+                product: { type: mongoose.Schema.Types.ObjectId, ref: 'products', required: true },
+                quantity: { type: Number, min: 1, default: 1 }
+            }
+        ]
+    },
+    { timestamps: true }
+);
 
 export const cartModel = mongoose.model(cartCollection, cartSchema);
