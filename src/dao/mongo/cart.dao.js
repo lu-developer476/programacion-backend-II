7f827fe757e6371d96839a2e37ac660c109d632a
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-08-29 21:56:50 -0300

    Delete src directory

--- src/dao/mongo/cart.dao.js
+++
@@ -1,7 +0,0 @@
-import { cartModel } from '../models/cart.model.js';
-
-export default class CartMongoDAO {
-    async getById(id) { return cartModel.findById(id).populate('products.product'); }
-    async create(data = {}) { return cartModel.create(data); }
-    async update(id, data) { return cartModel.findByIdAndUpdate(id, data, { new: true, runValidators: true }).populate('products.product'); }
-}
